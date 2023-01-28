862086061c2b7bf385af95c1f9f08d9981049a6f
Norbert <17ruud@gmail.com>
2023-01-28 20:50:28 +0100

    28.01

--- SQL/AnalyzingOlympicsDatabase/TopMedalsByCountry.sql
+++ SQL/AnalyzingOlympicsDatabase/TopMedalsByCountry.sql
@@ -26,7 +26,12 @@ JOIN sub3 ON sub2.noc = sub3.noc)
 
 SELECT olympics_history_noc_regions.region AS country, sub4.gold,sub4.silver,sub4.bronze  FROM sub4
 JOIN olympics_history_noc_regions ON olympics_history_noc_regions.noc = sub4.noc
-ORDER BY gold DESC, silver DESC, bronze DESC;
+ORDER BY gold DESC, silver DESC, bronze DESC
+
+LIMIT 10
+
+--RESULTS:
+
 
 
 
