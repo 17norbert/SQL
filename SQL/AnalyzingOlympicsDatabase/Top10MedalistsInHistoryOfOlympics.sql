17b5fd070007fa21995aeee130fcbfa733fd79ab
Norbert <17ruud@gmail.com>
2023-01-28 17:12:40 +0100

    Update Top10MedalistsInHistoryOfOlympics.sql

--- SQL/AnalyzingOlympicsDatabase/Top10MedalistsInHistoryOfOlympics.sql
+++ SQL/AnalyzingOlympicsDatabase/Top10MedalistsInHistoryOfOlympics.sql
@@ -1,3 +1,36 @@
+
+--- CREATE TABLE
+DROP TABLE IF EXISTS olympics_history;
+CREATE TABLE IF NOT EXISTS olympics_history
+(
+	id INT,
+	name VARCHAR,
+	sex VARCHAR,
+	age VARCHAR,
+	height VARCHAR,
+	weight VARCHAR,
+	team VARCHAR,
+	noc VARCHAR,
+	games VARCHAR,
+	year INT,
+	season VARCHAR,
+	city VARCHAR,
+	sport VARCHAR,
+	event VARCHAR,
+	medal VARCHAR
+);
+DROP TABLE IF EXISTS olympics_history_noc_regions;
+CREATE TABLE IF NOT EXISTS olympics_history_noc_regions
+(
+	noc VARCHAR,
+	region VARCHAR,
+	notes VARCHAR
+);
+
+---IMPORT DATA
+---
+---TOP 10 Countries with most medals in history
+
 WITH sub1 AS
 (SELECT name,COUNT(medal) as gold  FROM olympics_history
 WHERE medal NOT LIKE 'NA' AND medal LIKE 'Gold'
@@ -18,5 +51,7 @@ JOIN sub2 ON sub2.name = sub1.name
 JOIN sub3 ON sub2.name = sub3.name
 ORDER BY sub1.gold DESC,sub2.silver DESC,sub3.bronze DESC
 LIMIT 10
+
+
 
 
