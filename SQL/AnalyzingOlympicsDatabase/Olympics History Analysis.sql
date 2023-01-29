87604e0d909ba198c1f8d22845705dcb47786025
Norbert <17ruud@gmail.com>
2023-01-29 16:09:54 +0100

    29.01

--- SQL/AnalyzingOlympicsDatabase/Olympics History Analysis.sql
+++ SQL/AnalyzingOlympicsDatabase/Olympics History Analysis.sql
@@ -69,40 +69,41 @@ JOIN OLYMPICS_HISTORY_NOC_REGIONS ON OLYMPICS_HISTORY_NOC_REGIONS.NOC = SUB4.NOC
 ORDER BY GOLD DESC,
 	SILVER DESC,
 	BRONZE DESC
-LIMIT 10
+LIMIT 10;
 
 
 ---
 --- 2.TOP 10 medalists in history of Olympics
 
-WITH sub1 AS
-(SELECT name,COUNT(medal) as gold  FROM olympics_history
-WHERE medal NOT LIKE 'NA' AND medal LIKE 'Gold'
-GROUP BY name,medal),
+WITH sub1 AS 
+(
+SELECT name
+,COUNT(medal) as gold FROM olympics_history 
+WHERE medal NOT LIKE 'NA' AND medal LIKE 'Gold' 
+GROUP BY name, medal), 
+sub2 AS 
+(
+SELECT name, COUNT(medal) AS silver FROM olympics_history 
+WHERE medal NOT LIKE 'NA' AND medal LIKE 'Silver' 
+GROUP BY name, medal), 
+sub3 AS 
+(
+SELECT name, COUNT(medal) bronze FROM olympics_history 
+WHERE medal NOT LIKE 'NA' AND medal LIKE 'Bronze' 
+GROUP BY name,medal) 
 
-sub2 AS
-(SELECT name,COUNT(medal) AS silver  FROM olympics_history
-WHERE medal NOT LIKE 'NA' AND medal LIKE 'Silver'
-GROUP BY name,medal),
-
-sub3 AS
-(SELECT name,COUNT(medal) bronze  FROM olympics_history
-WHERE medal NOT LIKE 'NA' AND medal LIKE 'Bronze'
-GROUP BY name,medal)
-
-SELECT sub1.name,sub1.gold,sub2.silver,sub3.bronze FROM sub1
-JOIN sub2 ON sub2.name = sub1.name
-JOIN sub3 ON sub2.name = sub3.name
-ORDER BY sub1.gold DESC,sub2.silver DESC,sub3.bronze DESC
+SELECT sub1.name, sub1.gold, sub2.silver, sub3.bronze FROM sub1 
+JOIN sub2 ON sub2.name = sub1.name 
+JOIN sub3 ON sub2.name = sub3.name 
+ORDER BY sub1.gold DESC, sub2.silver DESC, sub3.bronze DESC 
 LIMIT 10;
 
 ---
---- 3.Top 10 sports with most representants in history
-SELECT sport,COUNT(DISTINCT(name)) AS total_number_of_players 
-FROM olympics_history
-GROUP BY sport
-ORDER BY total_number_of_players DESC
-LIMIT 10;
+--- 3.Sports with most representants in history
+SELECT sport, COUNT(DISTINCT(name)) AS total_number_of_players 
+FROM olympics_history 
+GROUP BY sport 
+ORDER BY total_number_of_players DESC 
 ---
 --- 4.Country with most medalists for each sport in history
 WITH sub1 AS
@@ -114,6 +115,7 @@ GROUP BY sport,region
 ORDER BY sport,total DESC
 )
 SELECT DISTINCT ON(sport) sport,region,total FROM sub1
+
 ---
 --- 5.Which team won the most medals in each olympics
 
